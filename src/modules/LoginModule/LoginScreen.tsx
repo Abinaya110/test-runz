a1fb9efbf4b8226f33f046d78f479873b653bd1d
Arulraj <abofficial1997@gmail.com>
2023-07-08 17:47:56 +0530

    fix select

--- src/modules/LoginModule/LoginScreen.tsx
+++ src/modules/LoginModule/LoginScreen.tsx
@@ -11,6 +11,7 @@ import LoginFrame from "./LoginFrame";
 import HelpAndTerms from "./HelpAndTerms";
 import { isEmpty, isValidEmail } from "../../utils/validators";
 import { useVisibilityIcon } from "../../utils/helpers";
+import SelectTag from "../../packages/SelectTag/SelectTag";
 
 type formType = {
   email: string;
@@ -47,64 +48,66 @@ const LoginScreen = () => {
     validate,
   });
   return (
-    <LoginFrame
-      leftChild={
-        <Flex between flex={1}>
-          <Flex>
-            <Text type={"h4"}>Welcome to</Text>
-            <Text type={"h2"}>Test Runz</Text>
-          </Flex>
+    <SelectTag />
 
-          <HelpAndTerms />
-        </Flex>
-      }
-      rightChild={
-        <Flex>
-          <Text type="title" className={styles.loginTitle}>
-            Log in to your Test Runz account
-          </Text>
-          <InputText
-            value={formik.values.email}
-            onChange={formik.handleChange("email")}
-            white
-            label={"E-mail"}
-            message={formik.errors.email}
-            status={formik.touched.email && formik.errors.email ? "error" : ""}
-          />
-          <div style={{ marginTop: 16, marginBottom: 8 }}>
-            <InputText
-              value={formik.values.password}
-              onChange={formik.handleChange("password")}
-              white
-              label={"Password"}
-              message={formik.errors.password}
-              status={
-                formik.touched.password && formik.errors.password ? "error" : ""
-              }
-              keyboardType={isVisible ? "text" : "password"}
-              actionRight={visibleIcon}
-            />
-          </div>
-          <Flex row center between>
-            <CheckBox label={"Remember me"} />
-            <Button onClick={handleForgot} types="link">
-              <Text color="shade-3">Forget your password?</Text>
-            </Button>
-          </Flex>
-          <Button className={styles.btnStyle} onClick={formik.handleSubmit}>
-            Log in
-          </Button>
-          <Flex row center>
-            <Text type="captionRegular">Don’t have an account yet? </Text>
-            <Button onClick={handleSignUp} types="link">
-              <Text type="captionBold" className={styles.signUpText}>
-                Click here to Sign up!
-              </Text>
-            </Button>
-          </Flex>
-        </Flex>
-      }
-    />
+    // <LoginFrame
+    //   leftChild={
+    //     <Flex between flex={1}>
+    //       <Flex>
+    //         <Text type={"h4"}>Welcome to</Text>
+    //         <Text type={"h2"}>Test Runz</Text>
+    //       </Flex>
+
+    //       <HelpAndTerms />
+    //     </Flex>
+    //   }
+    //   rightChild={
+    //     <Flex>
+    //       <Text type="title" className={styles.loginTitle}>
+    //         Log in to your Test Runz account
+    //       </Text>
+    //       <InputText
+    //         value={formik.values.email}
+    //         onChange={formik.handleChange("email")}
+    //         white
+    //         label={"E-mail"}
+    //         message={formik.errors.email}
+    //         status={formik.touched.email && formik.errors.email ? "error" : ""}
+    //       />
+    //       <div style={{ marginTop: 16, marginBottom: 8 }}>
+    //         <InputText
+    //           value={formik.values.password}
+    //           onChange={formik.handleChange("password")}
+    //           white
+    //           label={"Password"}
+    //           message={formik.errors.password}
+    //           status={
+    //             formik.touched.password && formik.errors.password ? "error" : ""
+    //           }
+    //           keyboardType={isVisible ? "text" : "password"}
+    //           actionRight={visibleIcon}
+    //         />
+    //       </div>
+    //       <Flex row center between>
+    //         <CheckBox label={"Remember me"} />
+    //         <Button onClick={handleForgot} types="link">
+    //           <Text color="shade-3">Forget your password?</Text>
+    //         </Button>
+    //       </Flex>
+    //       <Button className={styles.btnStyle} onClick={formik.handleSubmit}>
+    //         Log in
+    //       </Button>
+    //       <Flex row center>
+    //         <Text type="captionRegular">Don’t have an account yet? </Text>
+    //         <Button onClick={handleSignUp} types="link">
+    //           <Text type="captionBold" className={styles.signUpText}>
+    //             Click here to Sign up!
+    //           </Text>
+    //         </Button>
+    //       </Flex>
+    //     </Flex>
+    //   }
+    // />
   );
 };
 
---
+++ src/packages/SelectTag/SelectTag.tsx
@@ -0,0 +1,26 @@
+import styles from "./selecttag.module.scss";
+import { Select, Space } from "antd";
+
+const SelectTag = () => {
+  const handleChange = (value: string) => {
+    console.log(`selected ${value}`);
+  };
+
+  return (
+    <div className={styles.selectContainer}>
+      <Select
+        defaultValue="lucy"
+        style={{ width: 120 }}
+        onChange={handleChange}
+        options={[
+          { value: "jack", label: "Jack" },
+          { value: "lucy", label: "Lucy" },
+          { value: "Yiminghe", label: "yiminghe" },
+          { value: "disabled", label: "Disabled", disabled: true },
+        ]}
+      />
+    </div>
+  );
+};
+
+export default SelectTag;
