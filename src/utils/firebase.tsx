13a4ced85511a0393595cd2705184d991e1eb97d
Arulraj <abofficial1997@gmail.com>
2023-07-09 21:10:38 +0530

    fix ui

--- src/utils/firebase.tsx
+++ src/utils/firebase.tsx
@@ -4,13 +4,13 @@ import "firebase/compat/firestore";
 import { GoogleAuthProvider, OAuthProvider } from "firebase/auth";
 
 const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyA72za0r_67Y5dfyUXYZQNOFl3qx7j_0cI",
-  authDomain: "testrun-42bfa.firebaseapp.com",
-  projectId: "testrun-42bfa",
-  storageBucket: "testrun-42bfa.appspot.com",
-  messagingSenderId: "662343439569",
-  appId: "1:662343439569:web:4c0322973bcbcd013421f0",
-  measurementId: "G-P0WW852YHB",
+  apiKey: "AIzaSyDNkpEarzBcmZ7Bgc3OlaMV09y6wHUPseM",
+  authDomain: "testrunz-48ee2.firebaseapp.com",
+  projectId: "testrunz-48ee2",
+  storageBucket: "testrunz-48ee2.appspot.com",
+  messagingSenderId: "950793974939",
+  appId: "1:950793974939:web:11c011450141a2db8f823e",
+  measurementId: "G-ZKGYJXJZG1",
 };
 
 const firebaseConfig = FIREBASE_CONFIG;
